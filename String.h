5f6514afe67f2a4d8d6aa206c907c089737e7730
Judit Acs <judit@sch.bme.hu>
2020-03-23 19:41:09 +0100

    rename compare to isEqual

--- String.h
+++ String.h
@@ -9,18 +9,17 @@ namespace HomeMadeString
 		// Number of characters in the string
 		int elementNum;
 		// Pointer to the memory segment containing the characters
-		char*pData;
+		char* pData;
 	
 	public:
 		// Default constructor (without arguments):
-
 
 		// Copy constructor:
 
 
 		// Conversion constructor that expects a char* with '\0' at the end
 		// This constructor is responsible for the conversion char*-> String
-		String(const char* str);
+		String(char* str);
 
 		// Constructor with a char (c) and an int parameter (times)
 		// This constructor creates a string that contains 'times' pieces of the character c
@@ -29,13 +28,12 @@ namespace HomeMadeString
 
 		// Destructor:
 
-
 		// Copies the string to the buffer and terminates it with '\0' (string terminating character)
 		// Memory allocation for the buffer is to be done by a caller
-		void getStr(char * pBuff);
+		void getStr(char* pBuff);
 		
-		// Returns with the string length
-		int getLength(){return elementNum;}
+		// Returns with the string lengtgit@github.com:juditacs/bop2_lab06.gith
+		int getLength() { return elementNum; }
 
 		// Writes the string to the given stream		
 		void print(std::ostream& os);
@@ -48,8 +46,8 @@ namespace HomeMadeString
 		// Returns with a concatenated string
 		static String concatenate(String string1, String string2);
 
-		// Compares two strings
-		static bool compare(String string1, String string2);
+		// Returns true if the two strings are equal
+		static bool isEqual(String string1, String string2);
 
 		// Copies string2 to string1
 		static void copy(String& string1, String string2);
